6aecb1099c36afc985bc9bface3798545f584dc0
Daniel Tull <dt@danieltull.co.uk>
2020-06-27 13:58:29 +0100

    Fix test

--- Tests/PackageTests/PackageTests.swift
+++ Tests/PackageTests/PackageTests.swift
@@ -8,6 +8,6 @@ final class PackageTests: XCTestCase {
         let resources = try XCTUnwrap(Bundle.module.url(forResource: "Resources", withExtension: nil))
         let url = resources.appendingPathComponent("Test.txt")
         let package = try Package(url: url)
-        XCTAssertEqual(package.contents, "Hello World\n")
+        XCTAssertEqual(package.contents, "Hello World!\n")
     }
 }
