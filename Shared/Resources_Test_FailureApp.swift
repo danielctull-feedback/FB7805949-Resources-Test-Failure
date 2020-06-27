53507495e0f63e56cc90e65f146f72d55bfe33bf
Daniel Tull <dt@danieltull.co.uk>
2020-06-27 13:32:36 +0100

    Initial Commit

---
+++ Shared/Resources_Test_FailureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resources_Test_FailureApp.swift
+//  Shared
+//
+//  Created by Daniel Tull on 27.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct Resources_Test_FailureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
